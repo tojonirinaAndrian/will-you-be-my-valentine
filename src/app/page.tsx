689c617140a290657cbcd0da7c03b4306eccb9dc
Tojonirina Andrian <andrianjafiniaina21@gmail.com>
2026-02-09 14:45:30 +0300

    added name input to the first entry modal.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,131 +2,188 @@
 import Image from "next/image";
 import { useRef, useState, useTransition, useEffect } from "react";
 import { Howl } from "howler";
-import Head from "next/head";
 
 interface stageInterface {
-  noText: string,
-  gifLink: string, 
-  fontSize: number
+  noText: string;
+  gifLink: string;
+  fontSize: number;
 }
 
 export default function Home() {
   const stages: stageInterface[] = [
     {
       noText: "no",
-      gifLink: "/gifs/1.gif", 
-      fontSize: 0
-    }, {
+      gifLink: "/gifs/1.gif",
+      fontSize: 0,
+    },
+    {
       noText: "Really sure ?",
-      gifLink: "/gifs/2.gif", 
-      fontSize: 10
-    }, {
+      gifLink: "/gifs/2.gif",
+      fontSize: 10,
+    },
+    {
       noText: "Baby, pleeeasse ?",
-      gifLink: "/gifs/3.gif", 
-      fontSize: 20
-    }, {
+      gifLink: "/gifs/3.gif",
+      fontSize: 20,
+    },
+    {
       noText: "Just think about it",
-      gifLink: "/gifs/4.gif", fontSize: 25
-    }, {
+      gifLink: "/gifs/4.gif",
+      fontSize: 25,
+    },
+    {
       noText: "If you say no, I'll be sad",
-      gifLink: "/gifs/5.gif", fontSize: 40,
-    }, {
+      gifLink: "/gifs/5.gif",
+      fontSize: 40,
+    },
+    {
       noText: "Baby, Just say yes",
-      gifLink: "/gifs/6.gif", fontSize: 70
-    }, {
+      gifLink: "/gifs/6.gif",
+      fontSize: 70,
+    },
+    {
       noText: "",
-      gifLink: "/gifs/7.gif", fontSize: 0
-    }
+      gifLink: "/gifs/7.gif",
+      fontSize: 0,
+    },
   ];
 
   const [where, setWhere] = useState<number>(1);
   const onNoClick = () => {
-    setWhere(where+1)
+    setWhere(where + 1);
   };
 
   const onYesClick = () => {
     setWhere(7);
+    // TODO : send email
   };
-  
+
   const [trynaPlay, startPlaying] = useTransition();
   const [choosed, setChoosed] = useState<boolean>(false);
-  const  audioRef = useRef<Howl | null>(null);
+  const audioRef = useRef<Howl | null>(null);
+  const [name, setName] = useState<string>("");
+  const [musicOnOff, setMusicOnOff] = useState<boolean>(true);
   
   useEffect(() => {
     const mineAudio = new Howl({
       src: ["/audio/mine.mpeg"],
       volume: 0.5,
-      loop: true
+      loop: true,
     });
     audioRef.current = mineAudio;
   }, []);
 
-  const onPlayClick = () => {
-    console.log("will play the song now!");
-    startPlaying (() => {
-      audioRef.current?.play();
-      audioRef.current?.fade(0, 1, 1000);
-    });
+  const onContinueClick = () => {
+    musicOnOff &&
+      startPlaying(() => {
+        audioRef.current?.play();
+        audioRef.current?.fade(0, 1, 1000);
+      });
+    setChoosed(true);
   };
 
   return (
     <>
-    
-    {!choosed && <div className="w-screen h-screen bg-black/80 fixed top-0 right-0 flex justify-center items-center p-3">
-      <div className="bg-white p-5 space-y-3 rounded-xl w-full md:w-[30dvw] text-center">
-        <p className="font-2xl font-semibold">Play music ?</p>
-        <div className="w-full space-y-1 md:flex flex-col md:flex-row *:w-full *:rounded-md *:p-3 gap-1 *:cursor-pointer">
-          <button className="bg-green-400 text-green-900"
-          onClick={() => {
-            onPlayClick(); 
-            setChoosed(true)
-          }}
-          >Continue with music</button>
-          <button className="bg-blue-200 text-blue-700"
-          onClick={() => setChoosed(true)}
-          >Continue without music</button>
-        </div>
-      </div>
-    </div>}
-    <div
-    className="flex min-h-screen items-center justify-center dark:bg-black overflow-hidden p-10 bg-red-50"
-    >
-      {trynaPlay && <div className="text-center text-red-700 text-xl font-semibold">Music loading...</div>}
-      {(choosed && !trynaPlay) && <>
-        <main className="flex flex-col gap-3 justify-center items-center text-center">
-          <div className="md:w-[25dvw] w-[50dvw]">
-            {stages.map((stage) => {
-              return <Image 
-              priority
-              key={stage.gifLink} src={`${stage.gifLink}`} alt={`${stage.gifLink}`} width={500} height={500}
-              className={`${(stage.gifLink === stages[where-1].gifLink) ? "block" : "hidden"} w-full rounded-xl`}
+      {!choosed && (
+        <div className="w-screen h-screen bg-black/80 fixed top-0 right-0 flex justify-center items-center p-3">
+          <div className="bg-white p-5 space-y-3 rounded-xl w-full md:w-[30dvw]">
+            <div>
+              <p className="text-black/60">Name</p>
+              <input
+                onChange={(e) => setName(e.target.value.trim())}
+                type="text"
+                className="p-2 w-full border outline-red-400 border-red-100 bg-red-50/50 rounded-md"
+                placeholder="your name..."
               />
-            })}
+            </div>
+            <div>
+              <p className="text-black/60">Play with music ?</p>
+              <div className="flex w-full gap-1 *:p-2">
+                <button
+                  onClick={() => !musicOnOff && setMusicOnOff(true)}
+                  className={`w-full rounded-md border border-slate-600 ${musicOnOff ? "bg-slate-600 text-white" : "bg-slate-100/50 text-slate-600"}`}
+                >
+                  With music
+                </button>
+                <button
+                  onClick={() => musicOnOff && setMusicOnOff(false)}
+                  className={`w-full rounded-md border border-slate-600 ${!musicOnOff ? "bg-slate-600 text-white" : "bg-slate-100/50 text-slate-600"}`}
+                >
+                  Without music
+                </button>
+              </div>
+            </div>
+            <button
+              disabled={name.length > 0}
+              className={`w-full p-2 text-center bg-red-400 text-white rounded-md ${name.length <= 0 && "opacity-20"}`}
+              onClick={() => {
+                name.length > 0 && onContinueClick();
+              }}
+            >
+              Continue
+            </button>
           </div>
-          {(where !== 7) ? <>
-            <div className="w-screen">
-              <p className="font-semibold text-xl text-red-500">Will you be my valentine ?</p>
-            </div>
-            <div className="w-full p-3 justify-center flex gap-2 *:p-3 *:rounded-md *:text-white *:font-medium *:capitalize *:cursor-pointer *:px-5 *:h-fit items-center">
-              <button className={`bg-green-600`}
-              style={
-                {
-                  fontSize: `${where !== 1 && stages[where-1].fontSize}dvw`
-                }
-              }
-              onClick={onYesClick}
-              >Yes</button>
-              {(where !== 6) && <button className="bg-red-600"
-              onClick={onNoClick}
-              >{stages[where-1].noText}</button>}
-            </div>
-          </> : <>
-              <div className="text-center font-semibold text-6xl text-red-500">Thank you Malala</div>
-          </>}
-        </main>
-      </>}
-    </div>
+        </div>
+      )}
+      <div className="flex min-h-screen items-center justify-center dark:bg-black overflow-hidden p-10 bg-red-50">
+        {trynaPlay && (
+          <div className="text-center text-red-700 text-xl font-semibold">
+            Music loading...
+          </div>
+        )}
+        {choosed && !trynaPlay && (
+          <>
+            <main className="flex flex-col gap-3 justify-center items-center text-center">
+              <div className="md:w-[25dvw] w-[50dvw]">
+                {stages.map((stage) => {
+                  return (
+                    <Image
+                      priority
+                      key={stage.gifLink}
+                      src={`${stage.gifLink}`}
+                      alt={`${stage.gifLink}`}
+                      width={500}
+                      height={500}
+                      className={`${stage.gifLink === stages[where - 1].gifLink ? "block" : "hidden"} w-full rounded-xl`}
+                    />
+                  );
+                })}
+              </div>
+              {where !== 7 ? (
+                <>
+                  <div className="w-screen">
+                    <p className="font-semibold text-xl text-red-500">
+                      Will you be my valentine ?
+                    </p>
+                  </div>
+                  <div className="w-full p-3 justify-center flex gap-2 *:p-3 *:rounded-md *:text-white *:font-medium *:capitalize *:cursor-pointer *:px-5 *:h-fit items-center">
+                    <button
+                      className={`bg-green-600`}
+                      style={{
+                        fontSize: `${where !== 1 && stages[where - 1].fontSize}dvw`,
+                      }}
+                      onClick={onYesClick}
+                    >
+                      Yes
+                    </button>
+                    {where !== 6 && (
+                      <button className="bg-red-600" onClick={onNoClick}>
+                        {stages[where - 1].noText}
+                      </button>
+                    )}
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className="text-center font-semibold text-6xl text-red-500">
+                    Thank you Malala
+                  </div>
+                </>
+              )}
+            </main>
+          </>
+        )}
+      </div>
     </>
-    
   );
 }
